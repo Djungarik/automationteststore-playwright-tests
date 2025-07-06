6f19909f9824314dab177692264d176245f5c3d5
djungarik <berezhnoy.tim@gmail.com>
2025-07-06 13:17:45 +0300

    Update registration response handling

--- page-objects/RegisterPage.ts
+++ page-objects/RegisterPage.ts
@@ -43,12 +43,7 @@ export class RegisterPage {
     await this.page.locator("#AccountFrm_agree").click();
 
     await Promise.all([
-      this.page.waitForResponse(
-        (response) =>
-          response.url().includes("create") &&
-          response.request().method() === "POST" &&
-          response.status() === 302
-      ),
+      this.page.waitForURL("**/success"),
       this.page.getByRole("button", { name: "Continue" }).click(),
     ]);
   }
