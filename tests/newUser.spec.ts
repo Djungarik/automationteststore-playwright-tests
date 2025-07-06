82395e796191b038815cc093441ec957ede41594
djungarik <berezhnoy.tim@gmail.com>
2025-07-06 12:05:06 +0300

    Add a test for changing the user password

--- tests/newUser.spec.ts
+++ tests/newUser.spec.ts
@@ -8,7 +8,7 @@ test("register a new user", async ({ page, helperBase }, testInfo) => {
 
   await pm.navigateTo().loginOrRegister();
 
-  await page.getByRole("button", { name: "Continue" }).click();
+  await pm.onAccountLoginOrRegisterPage().startNewCustomerRegistration();
 
   const todaysDateAndTime = helperBase.getTodaysDateWithCurrentTime();
 
@@ -48,7 +48,7 @@ test("register a new user", async ({ page, helperBase }, testInfo) => {
     newUser.firstName
   );
 
-  await page.locator("[data-original-title='Logoff']").click();
+  await pm.onAccountPage().clickLogoffTile();
 
   await expect(
     page.getByText(
@@ -60,9 +60,78 @@ test("register a new user", async ({ page, helperBase }, testInfo) => {
 
   await pm.navigateTo().loginOrRegister();
 
-  await page.locator("#loginFrm_loginname").fill(loginName);
-  await page.locator("#loginFrm_password").fill(newUser.password);
-  await page.getByRole("button", { name: "Login" }).click();
+  await pm
+    .onAccountLoginOrRegisterPage()
+    .fillReturningCustomerForm(loginName, newUser.password);
+
+  await expect(page.locator(".heading1 .subtext")).toContainText(
+    newUser.firstName
+  );
+});
+
+test("user can change the password", async ({ page, helperBase }, testInfo) => {
+  testInfo.setTimeout(40_000);
+  const pm = new PageManager(page);
+
+  await pm.navigateTo().loginOrRegister();
+
+  await pm.onAccountLoginOrRegisterPage().startNewCustomerRegistration();
+
+  const todaysDateAndTime = helperBase.getTodaysDateWithCurrentTime();
+
+  const lastName = `ChangePassword-${newUser.lastName}-${todaysDateAndTime}`;
+  const baseEmail = newUser.email;
+  const [prefix, domain] = baseEmail.split("@");
+  const email = `${prefix}_${todaysDateAndTime}@${domain}`;
+  const loginName = `${newUser.loginName}${todaysDateAndTime}`;
+
+  await pm
+    .onRegisterPage()
+    .fillRegisterForm(
+      newUser.firstName,
+      lastName,
+      email,
+      newUser.telephone,
+      newUser.fax,
+      newUser.company,
+      newUser.address1,
+      newUser.address2,
+      newUser.city,
+      newUser.country,
+      newUser.zone,
+      newUser.zipcode,
+      loginName,
+      newUser.password,
+      newUser.subscribe.no
+    );
+
+  await expect(page.locator(".maintext")).toContainText(
+    "Your Account Has Been Created!"
+  );
+
+  await page.getByRole("link", { name: "Continue" }).click();
+
+  await pm.onAccountPage().clickChangePasswordTile();
+
+  await pm
+    .onChangePasswordPage()
+    .fillChangePasswordForm(
+      newUser.password,
+      newUser.newPassword,
+      newUser.newPassword
+    );
+
+  await expect(
+    page.getByText("Success: Your password has been successfully updated.")
+  ).toBeVisible();
+
+  await pm.onAccountPage().clickLogoffTile();
+
+  await pm.navigateTo().loginOrRegister();
+
+  await pm
+    .onAccountLoginOrRegisterPage()
+    .fillReturningCustomerForm(loginName, newUser.newPassword);
 
   await expect(page.locator(".heading1 .subtext")).toContainText(
     newUser.firstName
